c8ac5a5d6fd974a4760de3efec5c8e594a0d24b0
Falakrajpoot <itsmefalak2@gmail.com>
2024-03-04 14:12:23 +0500

    javascript document.write created.

---
+++ script.js
@@ -0,0 +1,5 @@
+document.write("I am the <b>master</b> of my own fate.");
+document.write("<br/>");
+document.write("I am the <b>champion</b> of my own soul.");
+document.write("<br/>");
+document.write("I am the <b>superhero</b> of my own life.");
